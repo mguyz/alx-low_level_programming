690750518ae7702c4c1898af35c6080ebc584927
mguyz <kllmungai@gmail.com>
2022-06-29 23:31:18 -0700

    fingerscrossed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,4 @@
-/**
-* main - prints the size of various types
-* Return: 0 if exited properly, non-zero otherwise
-*/
-
 int main() {
-	puts ("\"Programming is like building a multilingual puzzle");
-	return 0;
-	}
+puts ("Programming is like building a multilingual puzzle");
+return 0;
+}
